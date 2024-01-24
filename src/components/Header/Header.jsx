e423ea5a06d4b08f1e182fb2cc43e0af41e3fc84
AndrewSushi <AndrewSushi8@gmail.com>
2024-01-24 00:11:51 -0800

    Fixed the global header and nested routing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,28 @@
-import { Link } from "react-router-dom"
-import "./Header.css"
+import { Link, Outlet } from "react-router-dom"
 
 export default function Header(){
   return (
-    <header id="header">
-      <div className="logo">
-        <li><Link to="/">Home</Link></li>
-      </div>
-      <nav>
-        <ul id="links">
+    <>
+      <header id="header">
+        <div className="logo">
           <li><Link to="/">Home</Link></li>
-          <li><Link to={"books"}>Books</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/signin">Sign In</Link></li>
-        </ul>
-      </nav>
-      <div className="search-bar">
-        <input type="text" placeholder="Search for books"/>
-        <button type="button">Search</button>
+        </div>
+        <nav>
+          <ul id="links">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to={"books"}>Books</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/signin">Sign In</Link></li>
+          </ul>
+        </nav>
+        <div className="search-bar">
+          <input type="text" placeholder="Search for books"/>
+          <button type="button">Search</button>
+        </div>
+      </header>
+      <div>
+        <Outlet />
       </div>
-    </header>
+    </>
   )
 }
