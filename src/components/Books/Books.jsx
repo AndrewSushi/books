6cedc7b21d10be93549ce847ee32585172f6ed32
AndrewSushi <AndrewSushi8@gmail.com>
2024-01-24 18:59:05 -0800

    Added env variable for the api key and also added the basic fetching of the google books api

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react"
+import axios from "axios"
+
 export default function Books(){
+  const apiKey = import.meta.env.VITE_API_KEY
+  
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=balls&key=${apiKey}`
+      )
+      console.log(response)
+    }
+    fetchBooks()
+  }, [])
+
   return (
     <div id="books">
       <h1>Books!</h1>
