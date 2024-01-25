830fc56c7a377baddac0ae6b14a2ebbb4cc9cbc4
AndrewSushi <AndrewSushi8@gmail.com>
2024-01-24 22:36:10 -0800

    Added basic functionality for the api get request. Fetch the books, and then display them dynamically

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,22 +1,41 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 import axios from "axios"
 
 export default function Books(){
   const apiKey = import.meta.env.VITE_API_KEY
-  
+ 
+  const [books, setBooks] = useState([])
   useEffect(() => {
     const fetchBooks = async () => {
-      const response = await axios.get(
+      await axios.get(
         `https://www.googleapis.com/books/v1/volumes?q=balls&key=${apiKey}`
       )
-      console.log(response)
+        .then(response => {
+          setBooks(response.data.items)
+          console.log(response.data.items)
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
     fetchBooks()
-  }, [])
+  }, [apiKey])
 
   return (
     <div id="books">
       <h1>Books!</h1>
+      <ul>
+        {books && books.map(book => (
+          <li key={book.id}>
+            <p>
+              {book.volumeInfo.title}
+            </p>
+            {book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail && (
+              <img src={book.volumeInfo.imageLinks.thumbnail}/>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
