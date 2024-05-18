48a39d88b9ba03d1d417913dce03a5cfecc12869
AndrewSushi <andrewsushi8@gmail.com>
2024-05-17 23:14:05 -0700

    Consolidated code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,10 @@
-import { Link, Outlet, redirect } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 import { projectAuth } from "../../firebase/config.js"
 
 import firebase from 'firebase/compat/app';
 import "firebase/compat/auth";
 
 import { useAuthState } from "react-firebase-hooks/auth";
-// import { useCollectiveData } from "react-firebase-hooks/firestore"
-
-// const firestore = firebase.firestore()
 
 function SignIn(){
   const signInWithGoogle = () => {
@@ -20,19 +17,9 @@ function SignIn(){
   )
 }
 
-function SignOut(){
-  return projectAuth.currentUser && (
-    <button onClick={() => projectAuth.signOut()}>Sign Out</button>
-  )
-}
-
 async function Logout(){
   try {
     await projectAuth.signOut()
-
-    sessionStorage.removeItem('firebaseToken')
-    sessionStorage.removeItem('username')
-    return redirect("/")
   }catch(error){
     console.error('Error logging out', error)
   }
@@ -53,9 +40,7 @@ export default function Header(){
             <li><Link to="/books">Books</Link></li>
             {user && <li><Link to="/profile">Profile</Link></li>}
             {!user && <li><SignIn /></li>}
-            {/* {!user && <li><Link to="/sign_in">SIGN IN</Link></li>}
-            {!user && <li><Link to="/sign_up">SIGN UP</Link></li>} */}
-            {sessionStorage.username && <button onClick={Logout}>Log out</button>}
+            {user && <button onClick={Logout}>Log out</button>}
           </ul>
         </nav>
       </header>
